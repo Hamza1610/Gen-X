06b1bdbca255c7e17f2b0017c04a15b7245500aa
Hamza1610 <muhammadhamza162003@gmail.com>
2024-02-19 06:15:50 -0800

    spit code to info

---
+++ xyrox_engine/checker/risk_checker.py
@@ -0,0 +1,14 @@
+from engine import Xyrox
+
+engine = Xyrox()
+
+def check_risk():
+    """
+        Checks command risk
+    """
+    print('Command could be potentially risky. Exercise caution and double-check!')
+    engine.make_speech('The command you want to execute is potentially risky! should I go ahead and execute?')
+
+    speech = engine.listen_to_speech()
+    speech = speech.lower()
+    return speech
